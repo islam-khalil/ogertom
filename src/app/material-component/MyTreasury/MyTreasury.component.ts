c0de2046ad90dc339b9ff4062695b21e31ec7bdc
islam khalil <islam.khalil.elm@icloud.com>
2020-01-27 04:21:15 +0300

    initial

---
+++ src/app/material-component/MyTreasury/MyTreasury.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-MyTreasury',
+  templateUrl: './MyTreasury.component.html',
+  styleUrls: ['./MyTreasury.component.scss']
+})
+export class MyTreasuryComponent {
+  constructor() { }
+}
