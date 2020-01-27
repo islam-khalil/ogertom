842f2866f30b8416c831188715d717e7fa0a0783
islam-khalil <islam.khalil.elm@icloud.com>
2020-01-27 05:34:46 +0300

    add files

--- src/app/material-component/MyTreasury/MyTreasury.component.ts
+++ src/app/material-component/MyTreasury/MyTreasury.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./MyTreasury.component.scss']
 })
 export class MyTreasuryComponent {
+  tableColumns: string[] = ['NID', 'Name', 'Relation', 'Known'];
+  dataSource = [{ NID:'1010101010', Name: 'محمد سالم',Relation:'ابن', Known: 'نعم' },
+  { NID:'1210101010', Name: 'احمد سالم',Relation:'ابن', Known: 'نعم' },
+  { NID:'130101010', Name: 'عبد الجبار سالم',Relation:'اخ', Known: 'نعم' },
+  { NID:'1410101010', Name: 'هلا مسند  ',Relation:'زوجه', Known: 'لا' },
+  { NID:'1510101010', Name: 'شيخة العتيبي ',Relation:'زوجة', Known: 'نعم' }
+  ];
+
   constructor() { }
 }
